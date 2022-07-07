6088e34a6f10c5f870a1d04da16bc64e9c7f1449
saida93522 <54591143+saida93522@users.noreply.github.com>
2022-07-07 09:54:51 -0500

    docs:remove commented path

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.authenticate, name='auth_spotify'),
     path('home/', views.home,  name='home'),
-    # path('', views.get_weather_data,  name='weather'),
+    
     
 ]
